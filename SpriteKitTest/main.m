1485176c8109acf8f572581ac9db4250a108dcaa
Ewart Wigmans <EwartenMarieke@Ewart-Wigmanss-MacBook-Pro.local>
2013-12-09 19:39:40 +0100

    intial checkin

---
+++ SpriteKitTest/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  SpriteKitTest
+//
+//  Created by Ewart Wigmans on 13-11-13.
+//  Copyright (c) 2013 Ewart Wigmans. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "SpriteKitTestAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([SpriteKitTestAppDelegate class]));
+    }
+}
